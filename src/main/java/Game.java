e4f0b7ea82c9f70047acb83b923dca117b7dedd7
C-Edwards-26 <143653695+C-Edwards-26@users.noreply.github.com>
2026-04-21 10:14:59 -0700

    Finished initializing the grid!

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,13 +1,30 @@
 public class Game {
     private GameViewer frontend;
     private boolean running;
+    private Square[][] Grid;
+    public static int GRID_HEIGHT = 15;
+    public static int GRID_WIDTH = 25;
 
 
     public Game() {
-        frontend = new GameViewer(this);
-    }
-    public void startGame() {
+        Grid = new Square[GRID_HEIGHT][GRID_WIDTH];
 
+        for (int i = 0; i < GRID_HEIGHT; i++) {
+            for (int j = 0; j < GRID_WIDTH; j++) {
+                Grid[i][j] = new Square();
+            }
+        }
+
+        frontend = new GameViewer(this);
+        startGame();
+    }
+
+    public void startGame() {
+        frontend.repaint();
+    }
+
+    public Square[][] getGrid() {
+        return Grid;
     }
 
     public void gameLoop() {
